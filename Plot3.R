6f6607db4a4e954170ee15d61aec7be2d2524465
Nicholas Philo <67591185+nicholasphilo@users.noreply.github.com>
2020-07-24 15:47:48 +0100

    Created Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,50 @@
+### Loading Required packages
+library(lubridate)
+
+
+### Downloading data set if needed:
+
+
+if(  !file.exists("household_power_consumption.txt")  ){
+    
+    dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    
+    
+    download.file(dataset_url, destfile = "unzipme.zip", method = "curl")
+    unzip("unzipme.zip")
+    
+    
+}
+
+### Reading and conditioning the dataset:
+
+df <- read.table(  "household_power_consumption.txt" , sep = ";" , header = TRUE,
+                   colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),
+                   na.strings = "?"
+)
+
+con1 <-  which(  df$Date == "1/2/2007" | df$Date == "2/2/2007")
+df <- df[  con1  ,]
+
+### Creating a time variable:
+
+df$date_time <- paste(  df$Date , df$Time , sep = " "  )
+df$date_time <- parse_date_time(  df$date_time , orders =  "d/m/y H:M:S")
+
+
+
+########################################################################################################################################################################
+
+### Producing plot
+
+png(  "plot3.png"  )
+
+plot(df$date_time  ,  df$Sub_metering_1  , type = "l", xlab = "", ylab = "Energy sub metering")
+points(df$date_time  ,  df$Sub_metering_2  , type = "l", col = 2)
+points(df$date_time  ,  df$Sub_metering_3  , type = "l", col = 4  , )
+legend( x = "topright"   , legend = c("sub meter 1","sub meter 2","sub meter 3"), col = c(1,2,4) , lty = 1 )
+
+
+dev.off()
+
+
